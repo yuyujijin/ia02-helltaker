8868a938ff76ec3d0074c04fb0fad48aa80413a0
eugène <eugenevalty@gmail.com>
2022-06-14 18:56:43 +0200

    ajout d'heuristiques pour A*

--- src/search.py
+++ src/search.py
@@ -5,73 +5,102 @@ from typing import List, Tuple, Callable, Dict
 
 # BFS Search (Parcours en largeur)
 # Insert at the end of the list
-def insert_tail(s : State, L : List[State]) -> List[State]:
+def insert_tail(s: State, L: List[State]) -> List[State]:
     L.append(s)
     return L
 
 # Remove the head of the list
-def remove_head(L : List[State]) -> Tuple[State, List[State]]:
+def remove_head(L: List[State]) -> Tuple[State, List[State]]:
     if len(L) != 0:
-            elt = L.pop(0)
-            return(elt, L)
+        elt = L.pop(0)
+        return(elt, L)
     return (None, [])
 
 # DFS Search (Parcours en profondeur)
 # Insert at the head of the list
-def insert_prof(elt : State, L : List[State]) -> List[State]:
+def insert_prof(elt: State, L: List[State]) -> List[State]:
     L += [elt]
     return L
 
 # Remove the end of the list
-def remove_prof(L : List[State]) -> Tuple[State, List[State]]:
+def remove_prof(L: List[State]) -> Tuple[State, List[State]]:
     if len(L) != 0:
         elt = L.pop()
         return(elt, L)
     return (None, [])
 
 # A* Search
-# Factory for heuristic
-def heuristic_astar_factory(map_rules : Dict[str, set]) -> Callable[[State, int], int]:
+# Factories for heuristic
+def heuristic_manhattan_factory(map_rules: Dict[str, set]) -> Callable[[State, int], int]:
     # Manhattan
-    def heuristic_astar(elt : State, h : int) -> int:
-        (x,y) = elt.me
+    def heuristic_astar(elt: State, h: int) -> int:
+        (x, y) = elt.me
         (x_end, y_end) = list(map_rules['goals'])[0]
         h2 = abs(x - x_end) + abs(y - y_end)
         return h2
     return heuristic_astar
 
+# An advanced version of manhattan searching first for keys, then locks then goal
+def heuristic_manhattan_advanced_factory(map_rules: Dict[str, set]) -> Callable[[State, int], int]:
+    # Manhattan
+    def heuristic_astar(elt: State, h: int) -> int:
+        (x, y) = elt.me
+        # If there is a key go get it
+        if len(elt.keys) > 0:
+            (x_end, y_end) = list(elt.keys)[0]
+        # If there is a lock go get it
+        elif len(elt.locks) > 0:
+            (x_end, y_end) = list(elt.locks)[0]
+        # Else aim for the goal
+        else:
+            (x_end, y_end) = list(map_rules['goals'])[0]
+        h2 = abs(x - x_end) + abs(y - y_end)
+        return h2
+    return heuristic_astar
+# Euclidean
+def heuristic_euclidean_factory(map_rules: Dict[str, set]) -> Callable[[State, int], int]:
+    # Manhattan
+    def heuristic_astar(elt: State, h: int) -> int:
+        (x, y) = elt.me
+        (x_end, y_end) = list(map_rules['goals'])[0]
+        h2 = (x - x_end)**2 + (y - y_end)**2
+        return h2
+    return heuristic_astar
+
 # Insert in order of heuristics
-def insert_astar(elt : State, L : List[State]) -> List[State]:
+def insert_astar(elt: State, L: List[State]) -> List[State]:
     if L == []:
         return [elt]
 
     (_, h) = elt
 
-    i, n  = 0, len(L)
+    i, n = 0, len(L)
     while i < n:
         (_, h2) = L[i]
         if h <= h2:
             break
         i += 1
-    
+
     L = L[:i] + [elt] + L[i:]
     return L
 
 # Remove the head of the list
-def remove_astar(L : List[State]) -> Tuple[State, List[State]]:
+def remove_astar(L: List[State]) -> Tuple[State, List[State]]:
     if len(L) != 0:
-            elt = L.pop(0)
-            return(elt, L)
+        elt = L.pop(0)
+        return(elt, L)
     return (None, [])
 
 # Search with parent known (works with A* or not)
-def search_with_parent(s0 : State, 
-                       goals : Callable[[State], bool], 
-                       succ : Callable[[State], Dict[State, Action]], 
-                       remove : Callable[[List[State]], State], 
-                       insert : Callable[[State, List[State]], List[State]],
-                       heuristic : Callable[[Tuple[State, int]], int] = lambda s, h : 0,
-                       debug : bool = True) :
+def search_with_parent(s0: State,
+                       goals: Callable[[State], bool],
+                       succ: Callable[[State], Dict[State, Action]],
+                       remove: Callable[[List[State]], State],
+                       insert: Callable[[State, List[State]], List[State]],
+                       # Default heuristic just doesn't value anything
+                       heuristic: Callable[[
+                           Tuple[State, int]], int] = lambda s, h: 0,
+                       debug: bool = True):
     l = [(s0, 0)]
     save = {s0: None}
     s = s0
@@ -83,7 +112,7 @@ def search_with_parent(s0 : State,
         # Remove the head of the list with heuristics
         (s, h), l = remove(l)
         # For every possible successors
-        for s2 ,a in succ(s).items():
+        for s2, a in succ(s).items():
             # If exact same state is not already visited
             if not s2 in save:
                 # Save it
